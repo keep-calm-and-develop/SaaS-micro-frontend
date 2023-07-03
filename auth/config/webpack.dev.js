8330335334bf737f52057e6e71a4bbaa773c71a4
Aniket <aniket@nirmata.com>
2023-07-03 16:03:14 +0530

    Add Auth App
    1. Add signin and signup
    2. Configure webpack

---
+++ auth/config/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8082,
+        historyApiFallback: {
+            index: '/index.html',
+        },
+    },
+    output: {
+        publicPath: 'http://localhost:8082/',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthApp': './src/bootstrap',
+            },
+            shared: packageJson.dependencies,
+        }),
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
