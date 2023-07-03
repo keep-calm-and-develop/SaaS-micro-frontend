a72da3560cf6086269f7fcde7e256de6534184a1
Aniket <aniket@nirmata.com>
2023-07-03 16:04:07 +0530

    Integrate Auth app

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
@@ -13,9 +14,10 @@ const App = () => {
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <Header />
-                {/* Marketing App Starts here */}
-                <MarketingApp />
-                {/* Marketing App End here */}
+                <Switch>
+                    <Route path="/auth" component={AuthApp} />
+                    <Route path="/" component={MarketingApp} />
+                </Switch>
             </StylesProvider>
         </BrowserRouter>
     );
---
+++ container/src/components/AuthApp.js
@@ -0,0 +1,25 @@
+import { mount } from 'auth/AuthApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const AuthApp = () => {
+    const ref = useRef(null);
+    const history = useHistory();
+
+    useEffect(() => {
+        const { onParentNavigate } = mount(ref.current, {
+            initialPath: history.location.pathname,
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (history.location.pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
+    }, []);
+
+    return <div ref={ref} />;
+};
+
+export default AuthApp;
